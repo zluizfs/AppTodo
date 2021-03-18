6ace2fc2e2dcfd948b940bf1fc430338355e2f1c
Luiz <zluizfs@outlook.com>
2021-03-18 16:08:26 -0300

    Criação de TarefaServices e Modificações nas classes

--- src/main/java/br/edu/unipam/entity/Usuario.java
+++ src/main/java/br/edu/unipam/entity/Usuario.java
@@ -5,10 +5,15 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.NamedQuery;
 import javax.persistence.OneToMany;
 
 @Entity
+@NamedQuery(name = Usuario.GET_ALL_USERS, query = "select u from Usuario u order by u.nome")
 public class Usuario extends AbstractEntity {
+    
+    public static final String GET_ALL_USERS = "Usuario.getAllUsers";
+
     private String nome;
     private String email;
     
--- src/main/java/br/edu/unipam/service/UsuarioService.java
+++ src/main/java/br/edu/unipam/service/UsuarioService.java
@@ -1,6 +1,7 @@
 package br.edu.unipam.service;
 
 import br.edu.unipam.entity.Usuario;
+import java.util.List;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 
@@ -9,10 +10,41 @@ public class UsuarioService {
     @PersistenceContext(name = "pu_todo")
     private EntityManager entityManager;
     
+    // Inserir
     public Usuario salvarUsuario(Usuario usuario) {
         entityManager.persist(usuario);
         return usuario;
     }
+       
+    // Encontrar usuário por ID
+    public Usuario localizarPorId(Long id){
+        Usuario userBd = entityManager.find(Usuario.class, id);
+        return userBd;
+    }
     
+    // Editar
+    public Usuario editar(Usuario usuario){
+        Usuario userBd = localizarPorId(usuario.getId());
+        
+        if(userBd != null) {
+            entityManager.merge(usuario);
+            return usuario;
+        }
+        
+        return null;
+    }
     
+    // Remover
+    public void remover(Long id) {
+        Usuario user = localizarPorId(id);
+        if(user != null) {
+            entityManager.remove(user);
+        }
+    }
+    
+    // Localizar todos os usuários
+    public List<Usuario> listarTodos (){
+        // return entityManager.createQuery("select u from Usuario u order by nome", Usuario.class).getResultList();
+        return entityManager.createNamedQuery(Usuario.GET_ALL_USERS, Usuario.class).getResultList();
+    }
 }
